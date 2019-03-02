cccc4d7698830199822c4c2865b7b04cbcc6281d
Aaron Bawcom <abawcom@gmail.com>
2019-03-02 00:59:14 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Candid Labs',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Candid Labs provide Cloud Automation and DevOps software to plan, provision, and ensure cloud deployments are compliant',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
