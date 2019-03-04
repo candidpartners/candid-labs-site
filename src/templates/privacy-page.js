07655a690e5b5b51e47458d8c741e711e38d4b0f
Aaron Bawcom <abawcom@gmail.com>
2019-03-04 16:01:45 -0500

    Create privacy-page.js

---
+++ src/templates/privacy-page.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import Content, { HTMLContent } from '../components/Content'
+
+export const PrivacyPageTemplate = ({ title, content, contentComponent }) => {
+  const PageContent = contentComponent || Content
+
+  return (
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+              <PageContent className="content" content={content} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+PrivacyPageTemplate.propTypes = {
+  content: PropTypes.string,
+  contentComponent: PropTypes.func,
+}
+
+const PrivacyPage = ({ data }) => {
+  const { markdownRemark: post } = data
+
+  return (
+    <Layout>
+      <PrivacyPageTemplate
+        contentComponent={HTMLContent}
+        content={post.html}
+      />
+    </Layout>
+  )
+}
+
+PrivacyPage.propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
+export default PrivacyPage
+
+export const PrivacyPageQuery = graphql`
+  query PrivacyPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter
+    }
+  }
+`
