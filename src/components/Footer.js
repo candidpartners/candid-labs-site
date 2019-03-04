90a1d6fea88437457cb76b56a5fbbc82453e6b08
Aaron Bawcom <abawcom@gmail.com>
2019-03-04 15:57:04 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -31,8 +31,8 @@ const Footer = class extends React.Component {
                       </Link>
                     </li>
                     <li>
-                    <Link className="navbar-item" to="/contact/examples">
-                      Form Examples
+                    <Link className="navbar-item" to="/privacy">
+                      Privacy Policy
                     </Link>
                     </li>
                     <li><a
