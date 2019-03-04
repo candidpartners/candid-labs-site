337208a611bacdb12e7c590506d14cbd85e5f923
Aaron Bawcom <abawcom@gmail.com>
2019-03-04 16:19:14 -0500

    Create eula-page.js

---
+++ src/templates/eula-page.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import Content, { HTMLContent } from '../components/Content'
+
+export const EULAPageTemplate = ({ title, content, contentComponent }) => {
+  const PageContent = contentComponent || Content
+
+  return (
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+              <PageContent className="content" content={content} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+EULAPageTemplate.propTypes = {
+  content: PropTypes.string,
+  contentComponent: PropTypes.func,
+}
+
+const EULAPage = ({ data }) => {
+  const { markdownRemark: post } = data
+
+  return (
+    <Layout>
+      <EULAPageTemplate
+        contentComponent={HTMLContent}
+        content={post.html}
+      />
+    </Layout>
+  )
+}
+
+EULAPage.propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
+export default EULAPage
+
+export const EULAPageQuery = graphql`
+  query EULAPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+    }
+  }
+`
