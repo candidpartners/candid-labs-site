30d83fb806a41f713eb1db442103fd25d2f5b083
Kristie Patterson <kristiepatterson@CPT2-PATTERSON.local>
2019-03-20 09:10:52 -0400

    text edit

--- src/templates/product-page.js
+++ src/templates/product-page.js
@@ -71,14 +71,6 @@ export const ProductPageTemplate = ({
       <div className="columns">
         <div className="column is-10 is-offset-1">
           <div className="content">
-              <div className="columns" style={{ marginTop: '5em'}}>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    {heading}
-                  </h3>
-                  <p>{description}</p>
-                </div>
-              </div>
               <div className="columns">
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-3">
