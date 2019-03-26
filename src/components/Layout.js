6ff5a055683ffc96ac645b3ae10b04eb04c8802d
Kristie Patterson <kristiepatterson@CPT2-PATTERSON.local>
2019-03-26 08:55:12 -0400

    color fix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -33,7 +33,7 @@ const TemplateWrapper = ({ children }) => (
           <meta property="og:url" content="/" />
           <meta property="og:image" content="/img/volker-screen.png" />
           
-          <link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto" rel="stylesheet"/>\
+          <link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto" rel="stylesheet"/>
           <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous"/>
           <link
             rel="apple-touch-icon"
